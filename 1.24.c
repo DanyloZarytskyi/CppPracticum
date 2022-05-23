d54882b887065580ade713d71c32d886f30fd80e
DanyloZarytskyi <98876019+DanyloZarytskyi@users.noreply.github.com>
2022-05-23 21:45:17 +0300

    Add files via upload
    Danylo Zarytskyi Homework2 Statistics-1

---
+++ 1.24.c
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include<stdio.h>
+#include <iomanip>
+using namespace std;
+
+void main()
+{
+	int m; int n;
+
+	cout << "Enter m:";
+	cin >> m;
+	cout << "Enter n:";
+	cin >> n;
+
+	cout << setw(n+1) << m << endl;
+	cout << setw(2 * n+1) << m * m << endl;
+}
