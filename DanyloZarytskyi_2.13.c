51236c779d65587acba412d54b162f150f854347
DanyloZarytskyi <98876019+DanyloZarytskyi@users.noreply.github.com>
2022-05-23 23:02:55 +0300

    DanyloZarytskyi Homework3 Statistics1
    DanyloZarytskyi Homework3 Statistics1

---
+++ DanyloZarytskyi_2.13.c
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double r; double R; double pi;
+    pi = 3.14; double V;
+
+    printf("Enter r:");
+    scanf_s("%lf", &r);
+
+    printf("Enter R:");
+    scanf_s("%lf", &R);
+
+    V = (2 * pow(pi, 2) * pow(r, 2) * R);
+    printf("V = %lf", V);
+
+    return 0;
+}
+
