9a3b409a25415ea8e9cdf896034caf046e5d79a4
DanyloZarytskyi <98876019+DanyloZarytskyi@users.noreply.github.com>
2022-05-27 15:01:21 +0300

    Homework 6 Zarytskyi Statistics1
    Homework 6 Zarytskyi Statistics1

---
+++ Zarytskyi_4.24.c
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    std::cout << "Enter n:";
+    std::cin >> n;
+
+    std::stringstream ss;
+    ss << n;
+    std::string str = ss.str();
+    std::string result;
+
+    for (int i = 0; i < str.size(); ++i)
+        if (str[i] != '0' && str[i] != '5')
+            result.push_back(str[i]);
+    std::cout << result;
+
+}
---
+++ Zarytskyi_5.16e.c
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+
+
+int main()
+{
+	float x; float sum=0; int n;
+	
+	printf("Enter x(-1<x<1):");
+	scanf_s("%f", &x);
+
+	for (n = 0; n <= 10; n++)
+	{
+		if (fabs((pow(-1, n) * pow(x, n)) / n) > 0)
+			sum += (pow(-1, n) * pow(x, n)) / n;
+	}
+	printf("Sum=%f", sum);
+	return 0;
+
+}
