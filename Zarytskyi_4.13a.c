c087f6312157a577800782825aca4b011de9fad3
DanyloZarytskyi <98876019+DanyloZarytskyi@users.noreply.github.com>
2022-05-27 14:59:25 +0300

    Homework 5 Zarytskyi Statistics1
    Homework 5 Zarytskyi Statistics1

---
+++ Zarytskyi_4.13a.c
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+	int n; int x;
+	int sum = 0;
+
+	printf("Enter x:");
+	scanf_s("%d", &x);
+
+	printf("Enter n:");
+	scanf_s("%d", &n);
+
+
+	for (int i = 0; i <= n; i++)
+	{
+		sum = sum + pow((x - 1), i);
+	}
+	
+	printf("Result: %d", sum);
+
+	return 0;
+}
