83a4eb4ce0a458c46a711b5823ddbcc335f549f5
DanyloZarytskyi <98876019+DanyloZarytskyi@users.noreply.github.com>
2022-05-31 23:47:17 +0300

    Homework8 Zarytskyi Statistics1
    Homework8 Zarytskyi Statistics1

---
+++ Zarytskyi_7.13.c
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int arr[10]; int i, min, max, av, x, y, sum = 0, N = 10;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[" << i + 1 << "]" << ": ";
+        cin >> arr[i];
+    }
+
+    min = arr[0];
+    max = arr[0];
+    for (i = 0; i < N; i++)
+    {
+        sum += arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    av = sum / N;
+    for (i = 0; i < N; i++)
+    {
+        if (arr[i] > min && arr[i] < av) {
+            min = arr[i];
+            x = i;
+        }
+        if (arr[i] < max && arr[i] > av) {
+            max = arr[i];
+            y = i;
+        }
+    }
+
+    printf("Arithmetic mean is %d\n", av);
+    printf("and next values is A[%d] = %d and A[%d] = %d\n", x, min, y, max);
+
+    return 0;
+}
---
+++ Zarytskyi_7.8b.c
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    const int K = 100; int N; int arr[K]; int i; int j; int ct;
+    std::cout << "Enter N:\n";
+    std::cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cout << "[" << i + 1 << "]" << ": ";
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int j = 0; j < N; j++)
+    {
+        if (int(pow(arr[j], (1.0 / 3.0))) % 2 == 0)
+        {
+            sum+=1;
+        }
+    }
+
+    std::cout << "Number of full cubes:" << sum;
+
+}
